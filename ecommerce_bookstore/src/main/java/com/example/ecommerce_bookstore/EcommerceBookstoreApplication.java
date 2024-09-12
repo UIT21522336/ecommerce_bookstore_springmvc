85528573eccbacc5ddb0fbd5d44095af8a8d8f4d
UIT21522336 <21522336@gm.uit.edu.vn>
2024-09-12 16:16:03 +0700

    add maven project

---
+++ ecommerce_bookstore/src/main/java/com/example/ecommerce_bookstore/EcommerceBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBookstoreApplication.class, args);
+	}
+
+}
---
+++ ecommerce_bookstore/src/test/java/com/example/ecommerce_bookstore/EcommerceBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
